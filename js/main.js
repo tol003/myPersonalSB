c97034f7f141cd4451e492bc9322ef6d1c633814
tol003 <tol003@ucsd.edu>
2017-09-15 04:58:41 -0700

    sound and images added

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function playMusic(id){
   music = currArtist.getElementsByTagName('audio')[0];
 
   if(music.paused){
-    currArtist.getElementsByClassName('button-overlay')[0].style.backgroundImage = 'url(./playStuff/pauseCircle.png)';
+    currArtist.getElementsByClassName('button-overlay')[0].style.backgroundImage = 'url(../site-images/pauseCircle.png)';
     music.currentTime = 0;
     music.play();
 
@@ -25,7 +25,7 @@ $.ajax({
   }
 
   else{
-    currArtist.getElementsByClassName('button-overlay')[0].style.backgroundImage = 'url(./playStuff/playCircle.png)';
+    currArtist.getElementsByClassName('button-overlay')[0].style.backgroundImage = 'url(../site-images/playCircle.png)';
     music.pause();
   }
 }
