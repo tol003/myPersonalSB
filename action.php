92ad863f71d12039219f411cfc2bba7c28d9edbc
tol003 <tol003@ucsd.edu>
2017-09-15 01:26:10 -0700

    start of clean up

---
+++ action.php
@@ -0,0 +1,203 @@
+<?php
+  session_start();
+
+  require_once("connection.php");
+
+
+
+  function createLanding(){
+
+    $db = new Db();
+
+    $result = ($db->getPublicSB());
+
+    if(mysqli_num_rows($result) > 0){
+
+      while($row = mysqli_fetch_assoc($result)){
+
+         $str .= '<tr class="sb-box">
+                    <td class="table-row">
+                      <form class="form-button" action="./mySB/sounds.php" method="get">
+                        <input class="sb-button" type="submit" value="">
+                        <input class="hide" type="hidden" name="sbid" value="'. $row['board_id'] .'">
+                      </form>
+                    </td>
+                    <td>
+                      <form class="title-form">
+                        <input id="'. $row['board_id'] .'" name="sb-title" class=title-field type="text" value="'. $row['board_name'] .'" disabled>
+                        <input type="hidden" name="sbid" value="'. $row['board_id'] .'">
+                      </form>
+                    </td>
+                    <td>
+                      <form class="trash-container">
+                      </form>
+                    </td>
+                  </tr>';
+      }
+
+      echo $str;
+    }
+
+    else{
+      echo '<h2>There are no public Sound Boards</h2>';
+    }
+  }
+
+  /*function createAdminLanding(){
+
+    $db = new Db();
+
+    $result = ($db->getAllSB());
+
+    if(mysqli_num_rows($result) > 0){
+      while($row = mysqli_fetch_assoc($result)){
+
+         $str .= '<tr class="sb-box">
+                    <td class="table-row">
+                      <form class="form-button" action="/crud/private_sounds.php" method="get">
+                        <input class="sb-button" type="submit" value="">
+                        <input class="hide" type="hidden" name="sbid" value="'. $row['board_id'] .'">
+                      </form>
+                    </td>
+                    <td>
+                      <form class="title-form" action="/crud/sb_update_confirm.php" method="post">
+                        <input id="'. $row['board_id'] .'" name="sb-title" class=title-field type="text" value="'. $row['board_name'] .'">
+                        <input class="cancel-btn" type="button" value="">
+                        <input class="confirm-btn" type="submit" value="">
+                        <input type="hidden" name="sbid" value="'. $row['board_id'] .'">
+                      </form>
+                    </td>
+                    <td>
+                      <form class="trash-container" action="/crud/sb_delete_confirm.php" method="post">
+                        <input class="trash" type="submit" value="">
+                        <input type="hidden" name="sbid" value="'. $row['board_id'] .'">
+                        <input type="hidden" name="sb-title" value="'. $row['board_name'] .'">
+                        <input type="hidden" name="fromPage" value="adminIndex">
+                      </form>
+                    </td>
+                  </tr>';
+      }
+
+      $str .= '<form   style="text-align:center" action="/crud/private_SB.php" method="POST"  >
+                    <input type="text" name="sb_name" placeholder="Name" required>
+                    <br>
+                    <select name="status">
+                    <option value="0"> private </option>
+                    <option value="1"> public </option>
+                    </select> <br>
+                    <input title="Add SoundBoard" type="image" src="/crud/sb_images/addbtn.png">
+                  </form>';
+    }
+
+    echo $str;
+  }*/
+
+  function getSB($userID){
+
+    $db = new Db();
+
+    $result = ($db->userGetSB($userID));
+
+    if(mysqli_num_rows($result) > 0){
+      while($row = mysqli_fetch_assoc($result)){
+
+         $str .= '<tr class="sb-box">
+                    <td class="table-row">
+                      <form class="form-button" action="/crud/private_sounds.php" method="get">
+                        <input class="sb-button" type="submit" value="">
+                        <input class="hide" type="hidden" name="sbid" value="'. $row['board_id'] .'">
+                      </form>
+                    </td>
+                    <td>
+                      <form class="title-form" action="/crud/sb_update_confirm.php" method="post" onsubmit="return updateSBConfirm('. $row['board_id'] .')">
+                        <input id="'. $row['board_id'] .'" name="sb-title"
+                        class="title-field" type="text" value="'. $row['board_name'] .'">
+                        <input class="cancel-btn" type="button" value="">
+                        <input class="confirm-btn" type="submit" value="">
+                        <input type="hidden" name="sbid" value="'. $row['board_id'] .'">
+                      </form>
+                    </td>
+                    <td>
+                      <form class="trash-container" action="/crud/sb_delete_confirm.php" method="post" onsubmit="return deleteSBConfirm('. $row['board_id'] .')">
+                        <input class="trash" type="submit" value="">
+                        <input type="hidden" name="sbid" value="'. $row['board_id'] .'">
+                        <input type="hidden" name="sb-title" value="'. $row['board_name'] .'">
+                      </form>
+                    </td>
+                  </tr>';
+      }
+    }
+
+    $str .= '<form   style="text-align:center" action="/crud/private_SB.php" method="POST"  >
+                    <input type="text" name="sb_name" placeholder="Name" required>
+                    <br>
+                    <select name="status">
+                    <option value="0"> private </option>
+                    <option value="1"> public </option>
+                    </select> <br>
+                    <input title="Add SoundBoard" type="image" src="/crud/sb_images/addbtn.png">
+                  </form>';
+
+    echo $str;
+  }
+
+
+/************************* Get Sounds Functions ***************************/
+
+
+  function getSounds($sbID){
+
+    $db = new Db();
+
+    $result = ($db->userGetSound($sbID));
+
+    if(mysqli_num_rows($result) > 0){
+
+      while($row = mysqli_fetch_assoc($result)){
+        $str .= '<div class="button-wrapper" id="'. $row['sound_id'] .'" onclick="playMusic(this.id)">
+                 <audio src="'. $row['sound_path'] .'" preload="auto"></audio>
+                 <div class="button-overlay"></div>
+                 <img class="image-sound" src="'. $row['img_path'] .'">
+                 </div>';
+      }
+    }
+
+    echo $str;
+  }
+
+  function getPrivateSounds($sbID){
+
+    $db = new Db();
+
+    $result = ($db->userGetSound($sbID));
+
+    if(mysqli_num_rows($result) > 0){
+
+      while($row = mysqli_fetch_assoc($result)){
+        $str .= '<div sounds-wrapper>
+                   <div class="button-wrapper" id="'. $row['sound_id'] .'" onclick="playMusic(this.id)">
+                     <audio src="'. $row['sound_path'] .'" preload="auto"></audio>
+                     <div class="button-overlay"></div>
+                     <img class="image-sound" src="'. $row['img_path'] .'">
+                   </div>
+                   <form id="'. $row['sound_id'] .'" class="sound-button-del" action="/crud/sound_delete_confirm.php" method="post"
+                   onsubmit="return deleteSoundConfirm('. $row['sound_id'] .','. $sbID .')">
+                   <input class="sound-delete" type="submit" value="">
+                   <input type="hidden" name="sound_id" value="'. $row['sound_id'] .'">
+                   <input type="hidden" name="sound_path" value="'. $row['sound_path'] .'">
+                   <input type="hidden" name="sbid" value="'. $sbID .'">
+
+                   </form>
+                   <form class="sound-button-edit" action="/crud/soundupdate.php" method="get">
+                   <input class="sound-edit" type="submit" value="">
+                   <input type="hidden" name="sid" value="'. $row['sound_id'] .'">
+                   <input type="hidden" name="sbid" value="'. $sbID .'">
+                   </form>
+                 </div>';
+      }
+    }
+
+    echo $str;
+  }
+
+?>
--- landing.php
+++ landing.php
@@ -6,45 +6,57 @@
 <head>
   <meta charset="UTF-8">
   <title>v2 Soundboard</title>
-  <link rel="stylesheet" href="./sb.css">
-  <script src="./sb.js"></script>
+  <link rel="stylesheet" href="./css/landing.css">
 </head>
 <body>
   <div class="nav-container">
+    <div id="logo"></div>
     <ul>
-      <li><a href="/crud/index.php"><div>Soundboards</div></a>
+      <li><a href="./landing.php"><div>Soundboards</div></a>
         <div>
           <ul>
-            <li><a href="/crud/index.php">Public</a></li>
-            <?php
-              if($_SESSION['user_email']){
-                echo '<li><a href="/crud/private_SB.php">Private</a></li>';
-              }
-            ?>
+            <li><a href="./landing.php">Public</a></li>
+            <?php if(isset($_SESSION['user_email'])): ?>
+              <li><a href="./private_SB.php">Private</a></li>
+            <?php endif; ?>
           </ul>
         </div>
       </li>
-      <?php if (isset($_SESSION['user_email'])): ?>
+      <?php if(isset($_SESSION['user_email'])): ?>
         <li><a href="/crud/log_out.php"><div>Log Out</div></a>
         </li>
-      <?php endif ?>
+      <?php endif; ?>
       <?php if ($_SESSION['admin']=='1'): ?>
-        <li><a href="/crud/admin_page.php"><div>Admin</div></a>
+        <li><a href="./admin_page.php"><div>Admin</div></a>
         </li>
       <?php endif ?>
     </ul>
     <?php
-      if($_SESSION['user_email']){
+      if(isset($_SESSION['user_email'])){
         echo '<p class="login" id="hello">Hello, '. $_SESSION['user_first_name'] .'</p>';
       }
 
       else{
-        echo '<form id="register" action="/crud/registration.php" method="get">
+        echo '<form id="register" action="./registration.php" method="get">
           <input type="submit" value="Register">
         </form>
-        <form class="login" action="/crud/login.php" method="get">
-          <input type="submit" value="Login">
+        <form id="login" action="./login.php" method="get">
+          <input type="submit" value="Sign in">
         </form>';
       }
     ?>
-   </div>
+  </div>
+  <h1>Public Soundboards</h1>
+  <hr>
+  <table id="soundboard-container">
+    <tr id="table-heading">
+      <th id="sb-image">Board Images</th>
+      <th id="sb-title">Board Titles</th>
+      <th></th>
+    </tr>
+    <?php
+      require_once('action.php');
+      createLanding();
+    ?>
+</body>
+</html>
