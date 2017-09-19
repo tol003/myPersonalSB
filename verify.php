6930c0fabf83e8a6e4cb7b1d1cd17f821b9ac75a
tol003 <tol003@ucsd.edu>
2017-09-19 02:07:45 -0700

    registration checks completed

--- verify.php
+++ verify.php
@@ -1,8 +1,8 @@
 <?php
   session_start();
 
-  $user_req = '^\w{6,30}+$';
-  $pass_req = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z\d][a-zA-Z\d!@#$%^&*()_+]{7,}$';
+  $user_req = '/^\w{6,30}+$/';
+  $pass_req = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z\d][a-zA-Z\d!@#$%^&*()_+]{7,}$/';
   $user_input = $_POST['username'];
   $pass_input = $_POST['password'];
   $fname_input = $_POST['firstName'];
