c4d16bf508add35889e486796885d65fb5e6c6fc
tol003 <tol003@ucsd.edu>
2017-10-09 16:33:52 -0700

    fix nav for soundupload

--- soundupload.php
+++ soundupload.php
@@ -12,44 +12,42 @@ session_start();
 <body>
   <div class="nav-container">
     <ul>
-      <li><a href="/crud/index.php"><div>Soundboards</div></a>
+      <li><a href="./landing.php"><div>Soundboards</div></a>
         <div>
           <ul>
-            <li><a href="/crud/index.php">Public</a></li>
-            <?php
-              if($_SESSION['user_email']){
-                echo '<li><a href="/crud/private_SB.php">Private</a></li>';
-              }
-            ?>
+            <li><a href="./landing.php">Public</a></li>
+            <?php if(isset($_SESSION['email'])): ?>
+              <li><a href="./private_SB.php">Private</a></li>
+            <?php endif; ?>
           </ul>
         </div>
       </li>
-      <?php if (isset($_SESSION['user_email'])): ?>
-        <li><a href="/crud/log_out.php"><div>Log Out</div></a>
+      <?php if(isset($_SESSION['email'])): ?>
+        <li><a href="./logout.php"><div>Log Out</div></a>
         </li>
-      <?php endif ?>
+      <?php endif; ?>
       <?php if ($_SESSION['admin']=='1'): ?>
-        <li><a href="/crud/admin_page.php"><div>Admin</div></a>
+        <li><a href="./admin_page.php"><div>Admin</div></a>
         </li>
       <?php endif ?>
     </ul>
     <?php
-      if($_SESSION['user_email']){
-        echo '<p class="login" id="hello">Hello, '. $_SESSION['user_first_name'] .'</p>';
+      if(isset($_SESSION['email'])){
+        echo '<p class="login" id="hello">Hello, '. $_SESSION['first_name'] .'</p>';
       }
 
       else{
-        echo '<form id="register" action="/crud/registration.php" method="get">
+        echo '<form id="register" action="./registration.php" method="get">
           <input type="submit" value="Register">
         </form>
-        <form class="login" action="/crud/login.php" method="get">
-          <input type="submit" value="Login">
+        <form id="login" action="./login.php" method="get">
+          <input type="submit" value="Sign in">
         </form>';
       }
     ?>
-</div>
+  </div>
 <?php
-if($_SESSION['user_email'] ){
+if($_SESSION['email'] ){
 require_once("connection.php");
   if($_SERVER["REQUEST_METHOD"] == 'GET'){
   $sound_err = $_REQUEST["sound_err"];
@@ -157,12 +155,6 @@ $mime = mime_content_type($types);
 
    }
 
-
-
-
-
-
-
     if(!file_exists($_FILES['soundToUpload']['tmp_name']) || 
           !is_uploaded_file($_FILES['soundToUpload']['tmp_name'])){
           
@@ -249,7 +241,7 @@ $mime = mime_content_type($types);
 }
 }else{
 
-      Header("Location: login.php");
+      Header("Location: landing.php");
 
 }
 ?>
