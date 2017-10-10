ab37b9f5585b002c7a517514b376f074de400525
tol003 <tol003@ucsd.edu>
2017-10-10 04:20:03 -0700

    re-added admin page

--- admin_page.php
+++ admin_page.php
@@ -23,46 +23,37 @@
   <html>
     <head>
       <meta charset="utf-8">
-      <title>Admin Page</title>
-      <link rel="stylesheet" href="./sb_main.css">
-      <link rel="stylesheet" href="./sb.css">
-      <script src="./sb_main.js"></script>
+      <title>v2.1 Admin Page</title>
+      <?php
+        include('header.php');
+      ?>
     </head>
     <body>
       <div class="nav-container">
         <ul>
-          <li><a href="/crud/index.php"><div>Soundboards</div></a>
+          <li><a href="./landing.php"><div>Soundboards</div></a>
             <div>
               <ul>
-                <li><a href="/crud/index.php">Public</a></li>
-                <?php
-                  if($_SESSION['user_email']){
-                    echo '<li><a href="/crud/private_SB.php">Private</a></li>';
-                  }
-                ?>
+                <li><a href="./landing.php">Public</a></li>
+                <?php if(isset($_SESSION['email'])): ?>
+                  <li><a href="./private_SB.php">Private</a></li>
+                <?php endif; ?>
               </ul>
             </div>
           </li>
-          <?php if (isset($_SESSION['user_email'])): ?>
-            <li><a href="/crud/log_out.php"><div>Log Out</div></a>
+          <?php if(isset($_SESSION['email'])): ?>
+            <li><a href="./logout.php"><div>Log Out</div></a>
+            </li>
+          <?php endif; ?>
+          <?php if ($_SESSION['admin']=='1'): ?>
+            <li><a href="./admin_page.php"><div>Admin</div></a>
             </li>
           <?php endif ?>
         </ul>
         <?php
-          if($_SESSION['user_email']){
-            echo '<p class="login" id="hello">'. $_SESSION['user_first_name'] .'</p>';
-          }
-
-          else{
-            echo '<form id="register" action="/crud/registration.php" method="get">
-              <input type="submit" value="Register">
-            </form>
-            <form class="login" action="/crud/login.php" method="get">
-              <input type="submit" value="Login">
-            </form>';
-          }
+          echo '<p class="login" id="hello">Hello, '. $_SESSION['first_name'] .'</p>';
         ?>
-       </div>
+      </div>
       <?php
       $get_all_non_admin = "SELECT user_id, username, password, first_name, last_name, email FROM users WHERE admin = '0'";
       $user_table_result = $conn->query($get_all_non_admin);
